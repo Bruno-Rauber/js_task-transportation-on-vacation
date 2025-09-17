9fb418bbc20af9fa4f27519eda49dfaec3c6c0fc
Bruno Rauber <brunorauber.dev@gmail.com>
2025-09-17 11:27:46 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,15 +7,21 @@
 const DAILY_RATE = 40;
 const LONG_TERM_DISCOUNT = 50;
 const MID_TERM_DISCOUNT = 20;
+const MID_TERM_THRESHOLD = 3;
+const LONG_TERM_THRESHOLD = 7;
 
 function calculateRentalCost(days) {
-  if (days >= 3 && days <= 6) {
-    return days * DAILY_RATE - MID_TERM_DISCOUNT;
-  } else if (days >= 7) {
-    return days * DAILY_RATE - LONG_TERM_DISCOUNT;
-  } else {
-    return days * DAILY_RATE;
+  const basePrice = days * DAILY_RATE;
+
+  if (days >= LONG_TERM_THRESHOLD) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
+
+  if (days >= MID_TERM_THRESHOLD) {
+    return basePrice - MID_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
