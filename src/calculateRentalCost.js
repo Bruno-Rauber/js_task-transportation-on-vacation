14412d793b31ca026fe8541b5d05647e31f24a11
Bruno Rauber <brunorauber.dev@gmail.com>
2025-09-17 11:03:28 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 
-const dayPrice = 40;
-const desconto50 = 50;
-const desconto20 = 20;
+const DAILY_RATE = 40;
+const LONG_TERM_DISCOUNT = 50;
+const MID_TERM_DISCOUNT = 20;
 
 function calculateRentalCost(days) {
   if (days >= 3 && days <= 6) {
-    return days * dayPrice - desconto20;
+    return days * DAILY_RATE - MID_TERM_DISCOUNT;
   } else if (days >= 7) {
-    return days * dayPrice - desconto50;
+    return days * DAILY_RATE - LONG_TERM_DISCOUNT;
   } else {
-    return days * dayPrice;
+    return days * DAILY_RATE;
   }
 }
 
